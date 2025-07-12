174a1d9dc5a3c0c8305e33c5e5d2c1c77b7184c7
Alin528491 <alin@alinalin.net>
2025-07-11 21:35:52 -0400

    V4.7

--- script.js
+++ script.js
@@ -185,7 +185,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 labels: ["7:00", "9:00", "11:00", "13:00", "15:00", "17:00", "19:00"],
                 datasets: [{
                     label: `${location.replace("-", " ")} ABG Density`,
-                    data: [1, 3, 5, 7, 9, 6, 4],
+                    data: [1, 3, 3, 3, 2, 1, 0],
                     borderColor: "red",
                     borderWidth: 2,
                     fill: false
